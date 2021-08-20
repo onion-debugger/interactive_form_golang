0c0a00afe4c261987e313c0eba233160abf5a96d
oshiole <oshiole94@gmail.com>
2021-08-20 21:13:02 +0100

    Added server file and front end file

---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+const confirmationMessage = `<h1>Confirmation</h1>
+<p>Your message has been sent!</p>`
+
+// routing function contactHandler() for handling form submission
+func contactHandler(writer http.ResponseWriter, request *http.Request) {
+	// handling route security
+	routeSecurity(writer, request)
+
+	if err := request.ParseForm(); err != nil {
+		fmt.Fprintf(writer, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintln(writer, confirmationMessage)
+	name := request.FormValue("name")
+	email := request.FormValue("email")
+	phoneNumber := request.FormValue("phone")
+	message := request.FormValue("message")
+
+	fmt.Fprintf(writer, "Name: %s\n", name)
+	fmt.Fprintf(writer, "Email: %s\n", email)
+	fmt.Fprintf(writer, "Phone Number: %s\n", phoneNumber)
+	fmt.Fprintf(writer, "Message: %s\n", message)
+	
+}
+
+// Handling route Security
+func routeSecurity(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		http.Error(writer, "<p>This Page doesn't exist</p>", http.StatusNotFound)
+		return
+	}
+
+	if request.Method != "POST" {
+		http.Error(writer, "We cannot process this request", http.StatusNotFound)
+		return
+	}
+}
+
+
+func main() {
+	// loading static file
+	homePage := http.FileServer(http.Dir("./static"))
+	http.Handle("/", homePage)
+
+	http.HandleFunc("/submitContact", contactHandler)
+
+	fmt.Println("Server started at port 8080")
+
+	// Handling error and starting server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
